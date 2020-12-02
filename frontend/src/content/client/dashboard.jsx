58f43c78e937bca018990cbad0b73c7952e32ad0
bertmeeuws <bert.meeuws@student.howest.be>
2020-12-02 19:10:18 +0100

    A user can answers his questionnaire

--- frontend/src/content/client/dashboard.jsx
+++ frontend/src/content/client/dashboard.jsx
@@ -1,19 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/client.css";
 import Questionnaire from "../../components/Questionnaire/Questionnaire";
 import MobileHeader from "../../components/MobileHeader/MobileHeader";
 import { useStoreState } from "easy-peasy";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation, useLazyQuery } from "@apollo/client";
 import { LoaderLarge } from "../../components/Loaders/Loaders";
 import CoachPopup from "../../components/AddCoach/Index";
 
 const GET_USER_DATA = gql`
-  query MyQuery($id: Int!) {
-    client(where: { user_id: { _eq: $id } }) {
+  query MyQuery($id: Int!, $date: date!) {
+    client(where: { user_id: { _eq: $id } }, limit: 1) {
       name
       weight
       coach_id
       surname
+      user {
+        surveys(limit: 1, where: { created_at: { _eq: $date } }) {
+          id
+          energy_workout
+          energy_day
+          craving
+        }
+      }
+    }
+  }
+`;
+
+const INSERT_EMPTY_SURVEY = gql`
+  mutation InsertEmptySurvey($object: survey_insert_input!) {
+    insert_survey_one(object: $object) {
+      user_id
+      energy_day
+      energy_workout
+      craving
+    }
+  }
+`;
+
+const UPDATE_SURVEY = gql`
+  mutation UpdateSurvey(
+    $user_id: Int_comparison_exp!
+    $date: date!
+    $energy_day: Int!
+    $energy_workout: Int!
+    $craving: Int!
+  ) {
+    update_survey(
+      where: { _and: { created_at: { _eq: $date } }, user_id: $user_id }
+      _set: {
+        energy_day: $energy_day
+        energy_workout: $energy_workout
+        craving: $craving
+      }
+    ) {
+      returning {
+        energy_day
+        energy_workout
+        craving
+      }
+    }
+  }
+`;
+
+const CHECK_IF_USER_HAS_SURVEY = gql`
+  query CheckIfUserHasSurvey($user_id: Int!, $date: date!) {
+    survey(
+      where: {
+        user_id: { _eq: $user_id }
+        _and: { created_at: { _eq: $date } }
+      }
+    ) {
+      id
+      energy_day
+      energy_workout
+      craving
     }
   }
 `;
@@ -26,19 +86,87 @@ export default function Dashboard() {
 
   const id = useStoreState((state) => state.user_id);
 
+  const [INSERT_EMPTY_SURVEY_FOR_USER] = useMutation(INSERT_EMPTY_SURVEY);
+  const [UPDATE_SURVEY_USER] = useMutation(UPDATE_SURVEY);
+
+  const getDate = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+
+    return (today = yyyy + "-" + mm + "-" + dd);
+  };
+
+  const [fetchForSurveys, { data: hasSurvey }] = useLazyQuery(
+    CHECK_IF_USER_HAS_SURVEY,
+    {
+      variables: {
+        user_id: id,
+        date: getDate(),
+      },
+    }
+  );
+
+  //const { data: survey } = useQuery(CHECK_IF_USER_HAS_SURVEY);
+
   const { data, errors, loading } = useQuery(GET_USER_DATA, {
     variables: {
       id: id,
+      date: getDate(),
     },
   });
 
-  if (data) {
-    console.log(data.client[0].surname);
-  }
+  useEffect(async () => {
+    if (hasSurvey) {
+      console.log(hasSurvey);
+      setEnergyWorkout(hasSurvey.survey[0].energy_workout);
+      setEnergyDay(hasSurvey.survey[0].energy_day);
+      setCraving(hasSurvey.survey[0].craving);
+    } else {
+      await fetchForSurveys();
+    }
+  }, [hasSurvey]);
 
   if (!data) {
     return <LoaderLarge />;
   }
+
+  const addAnswersToDatabase = async (e) => {
+    e.preventDefault();
+    //if there is no survey in the database for today, create one first.
+
+    if (hasSurvey.survey.length === 0) {
+      console.log("No survey yet");
+      const { data: created_item } = await INSERT_EMPTY_SURVEY_FOR_USER({
+        variables: {
+          object: {
+            user_id: id,
+            energy_day: 3,
+            energy_workout: 3,
+            craving: 3,
+          },
+        },
+      });
+      console.log(created_item);
+    } else {
+      //update survey from user
+      console.log("Updating survey");
+      console.log(craving + " " + energyWorkout + " " + energyDay);
+      const { data, errors } = await UPDATE_SURVEY_USER({
+        variables: {
+          user_id: { _eq: id },
+          date: getDate(),
+          energy_day: Number(energyDay),
+          energy_workout: Number(energyWorkout),
+          craving: Number(craving),
+        },
+      });
+      console.log(data);
+    }
+
+    //if there is one for today, update with the values
+  };
 
   return (
     <>
@@ -66,13 +194,22 @@ export default function Dashboard() {
           <input className="client-weight-input" type="text" />
           <button className="client-weight-button">Confirm</button>
         </article>
-        <article className="client-questionnaire rounded shadow">
+        <form
+          onSubmit={addAnswersToDatabase}
+          className="client-questionnaire rounded shadow"
+        >
           <h1 className="hidden">Questionnaire</h1>
           <div className="cravings">
             <h2 className="questionnaire-name">Cravings</h2>
             <div>
               <p className="questionnaire-ratings">none</p>
-              <Questionnaire value={craving} set={setCraving} name="cravings" />
+              <Questionnaire
+                value={craving}
+                set={(value) => {
+                  setCraving(value);
+                }}
+                name="cravings"
+              />
               <p className="questionnaire-ratings">a lot</p>
             </div>
           </div>
@@ -80,7 +217,13 @@ export default function Dashboard() {
             <h2 className="questionnaire-name">Energy during workout</h2>
             <div>
               <p className="questionnaire-ratings">low</p>
-              <Questionnaire value={energyDay} set={setEnergyDay} name="day" />
+              <Questionnaire
+                value={energyWorkout}
+                set={(value) => {
+                  setEnergyWorkout(value);
+                }}
+                name="day"
+              />
               <p className="questionnaire-ratings">high</p>
             </div>
           </div>
@@ -89,14 +232,17 @@ export default function Dashboard() {
             <div>
               <p className="questionnaire-ratings">low</p>
               <Questionnaire
-                value={energyWorkout}
-                set={setEnergyWorkout}
+                value={energyDay}
+                set={(value) => {
+                  setEnergyDay(value);
+                }}
                 name="workout"
               />
               <p className="questionnaire-ratings">high</p>
             </div>
           </div>
-        </article>
+          <input type="submit" value="submit" />
+        </form>
         <article className="client-workout">
           <h1 className="client-workout-title">Today's workout</h1>
           <table className="dashboard-workout shadow rounded">
